32577b1411ecb9c8ab228404d73d0a8d8787b306
Eric Funk <psytraxx@gmail.com>
2025-10-24 08:57:58 +0200

    fix: update syn and other dependencies in Cargo.lock; refactor Hub75Peripherals to use AnyPin

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ use display::{
 use embassy_executor::Spawner;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
 use embassy_time::{Duration, Timer};
-use esp_alloc::heap_allocator;
 use esp_backtrace as _;
 use esp_hal::{
+    gpio::Pin,
     i2c::master::{Config, I2c},
+    interrupt::{software::SoftwareInterruptControl, Priority},
     rng::Rng,
     time::Rate,
     timer::timg::TimerGroup,
@@ -21,7 +22,8 @@ use esp_hal::{
 };
 use esp_hub75::framebuffer::{compute_frame_count, compute_rows, plain::DmaFrameBuffer};
 use esp_println::{logger::init_logger, println};
-use esp_rtos::main;
+use esp_rtos::embassy::InterruptExecutor;
+use log::info;
 use wifi_task::{connect_to_wifi, shutdown_wifi};
 
 mod clock;
@@ -61,11 +63,13 @@ pub(crate) trait ClockfaceTrait {
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[main]
+#[esp_rtos::main]
 async fn main(spawner: Spawner) {
     init_logger(log::LevelFilter::Info);
 
     let peripherals = esp_hal::init(esp_hal::Config::default());
+    let sw_ints = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    let software_interrupt = sw_ints.software_interrupt2;
 
     // --- RTC Initialization Start ---
     println!("Initializing I2C for BM8563 RTC...");
@@ -74,8 +78,6 @@ async fn main(spawner: Spawner) {
         .expect("Unable to create I2C instance")
         .with_scl(peripherals.GPIO42)
         .with_sda(peripherals.GPIO41);
-
-    heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 73744);
 
     let mut clock_buffs = ClockBuffs::default();
     let mut clock = Clock::<I2CType>::new(i2c);
@@ -91,32 +93,63 @@ async fn main(spawner: Spawner) {
     println!("init framebuffers");
     let fb0 = mk_static!(FBType, FBType::new());
     let fb1 = mk_static!(FBType, FBType::new());
-    fb0.erase();
-    fb1.erase();
+
+    info!("fb0: {:?}", fb0);
+    info!("fb1: {:?}", fb1);
 
     let hub75_peripherals = Hub75Peripherals {
         lcd_cam: peripherals.LCD_CAM,
         dma_channel: peripherals.DMA_CH0,
-        red1: peripherals.GPIO2,
-        grn1: peripherals.GPIO6,
-        blu1: peripherals.GPIO10,
-        red2: peripherals.GPIO3,
-        grn2: peripherals.GPIO7,
-        blu2: peripherals.GPIO11,
-        addr0: peripherals.GPIO39,
-        addr1: peripherals.GPIO38,
-        addr2: peripherals.GPIO37,
-        addr3: peripherals.GPIO36,
-        addr4: peripherals.GPIO21,
-        blank: peripherals.GPIO35,
-        clock: peripherals.GPIO34,
-        latch: peripherals.GPIO33,
+        red1: peripherals.GPIO2.degrade(),
+        grn1: peripherals.GPIO6.degrade(),
+        blu1: peripherals.GPIO10.degrade(),
+        red2: peripherals.GPIO3.degrade(),
+        grn2: peripherals.GPIO7.degrade(),
+        blu2: peripherals.GPIO11.degrade(),
+        addr0: peripherals.GPIO39.degrade(),
+        addr1: peripherals.GPIO38.degrade(),
+        addr2: peripherals.GPIO37.degrade(),
+        addr3: peripherals.GPIO36.degrade(),
+        addr4: peripherals.GPIO21.degrade(),
+        blank: peripherals.GPIO35.degrade(),
+        clock: peripherals.GPIO34.degrade(),
+        latch: peripherals.GPIO33.degrade(),
     };
 
-    spawner
-        .spawn(hub75_task(hub75_peripherals, &RX, &TX, fb1))
-        .unwrap();
-    spawner.spawn(display_task(&TX, &RX, fb0)).unwrap();
+    // run hub75 and display on second core
+    let cpu1_fnctn = {
+        move || {
+            use esp_rtos::embassy::Executor;
+            let hp_executor = mk_static!(
+                InterruptExecutor<2>,
+                InterruptExecutor::new(software_interrupt)
+            );
+            let high_pri_spawner = hp_executor.start(Priority::Priority3);
+
+            // hub75 runs as high priority task
+            high_pri_spawner
+                .spawn(hub75_task(hub75_peripherals, &RX, &TX, fb1))
+                .ok();
+
+            let lp_executor = mk_static!(Executor, Executor::new());
+            // display task runs as low priority task
+            lp_executor.run(|spawner| {
+                spawner.spawn(display_task(&TX, &RX, fb0)).ok();
+            });
+        }
+    };
+
+    use esp_hal::system::Stack;
+    const DISPLAY_STACK_SIZE: usize = 8192;
+    let app_core_stack = mk_static!(Stack<DISPLAY_STACK_SIZE>, Stack::new());
+
+    esp_rtos::start_second_core(
+        peripherals.CPU_CTRL,
+        sw_ints.software_interrupt0,
+        sw_ints.software_interrupt1,
+        app_core_stack,
+        cpu1_fnctn,
+    );
 
     let rng = Rng::new();
     let seed = (rng.random() as u64) << 32 | rng.random() as u64;
